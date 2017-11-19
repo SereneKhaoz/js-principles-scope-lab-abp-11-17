198c3ba84feeeaf3641917ff3b503214232d6552
Dynisha Faust <dgeekgirl08@gmail.com>
2017-11-19 16:42:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Write your solution in this file!
 var customerName = 'bob';
 
-function upperCaseCustomerName(string){
+function upperCaseCustomerName("bob"){
   return customerName.toUpperCase();
 };
